533d0c1e2c40587c3410e902f659a5494f7c0a16
Ivan <djsupersky@gmail.com>
2022-06-02 16:17:54 +0300

    add swagger

--- src/main/java/com/ivanxc/netcracker/bookshop/BookShopApplication.java
+++ src/main/java/com/ivanxc/netcracker/bookshop/BookShopApplication.java
@@ -1,9 +1,12 @@
 package com.ivanxc.netcracker.bookshop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Book shop API", version = "1.0.0", description = "Book shop web service"))
 public class BookShopApplication {
 
     public static void main(String[] args) {
